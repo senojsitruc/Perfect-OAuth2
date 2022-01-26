bc280715cb68370a525fbe36fb5f255e7a1e44e3
Curtis Jones <curtis.jones@gmail.com>
2022-01-26 18:17:36 -0500

    Updates package with platform versions.

--- Package.swift
+++ Package.swift
@@ -3,25 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "OAuth2",
-		platforms: [
-			.iOS(.v10),
-			.macOS(.v10_10),
-			.tvOS(.v11)
-		],
-    products: [
-        .library(
-            name: "OAuth2",
-            targets: ["OAuth2"]),
-    ],
-    dependencies: [
-        .package(url: "http://10.10.10.155:3000/senojsitruc/Perfect-Session.git", branch: "senojsitruc"),
-    ],
-    targets: [
-        .target(
-            name: "OAuth2", dependencies: [
-							.product(name: "PerfectSession", package: "Perfect-Session")
-						]
-				)
-		]
+	name: "OAuth2",
+	platforms: [
+		.iOS(.v10),
+		.macOS(.v10_10),
+		.tvOS(.v11)
+	],
+	products: [
+		.library(name: "OAuth2", targets: ["OAuth2"]),
+	],
+	dependencies: [
+		.package(url: "http://10.10.10.155:3000/senojsitruc/Perfect-Session.git", branch: "senojsitruc"),
+	],
+	targets: [
+		.target(name: "OAuth2", dependencies: [
+			.product(name: "PerfectSession", package: "Perfect-Session")
+		])
+	]
 )
